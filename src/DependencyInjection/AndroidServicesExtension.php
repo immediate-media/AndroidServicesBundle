ebda05a79e377ebacb8cd23bf75337fb374053ee
alex.ferreira <alex.ferreira@immediate.co.uk>
2025-02-12 14:53:56 +0000

    fix: update AndroidServicesExtension.php
    - refract code causing the bundle not to install properly

--- src/DependencyInjection/AndroidServicesExtension.php
+++ src/DependencyInjection/AndroidServicesExtension.php
@@ -6,18 +6,23 @@ use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class AndroidServicesExtension extends Extension
 {
-
     /**
      * @throws Exception
      * @SuppressWarnings(PHPMD)
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
         $loader->load('services.yaml');
     }
 }
