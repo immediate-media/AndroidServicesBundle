2d3c3bbb58557c6d94550d97d981c1e0822a2f4c
alex.ferreira <alex.ferreira@immediate.co.uk>
2025-03-31 10:08:01 +0100

    fix: phpcs update

--- src/AndroidServicesApi.php
+++ src/AndroidServicesApi.php
@@ -37,7 +37,7 @@ class AndroidServicesApi
 
     /**
      * @throws AndroidServiceException|JsonException
-     * @phpcs:ignore
+     * @phpcs:disable
      * @link https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptionsv2 Purchases.subscriptionsV2
      * @phpcs:enable
      * */
@@ -68,7 +68,7 @@ class AndroidServicesApi
 
     /**
      * @throws AndroidServiceException|JsonException
-     * @phpcs:ignore
+     * @phpcs:disable
      * @link https://developers.google.com/android-publisher/api-ref/rest/v3/monetization.subscriptions.basePlans.offers Monetization.subscriptions.basePlans.offers
      * @phpcs:enable
      * */
@@ -103,7 +103,7 @@ class AndroidServicesApi
 
     /**
      * @throws AndroidServiceException|JsonException
-     * @phpcs:ignore
+     * @phpcs:disable
      * @link https://developers.google.com/android-publisher/api-ref/rest/v3/monetization.subscriptions Monetization.subscriptions
      * @phpcs:enable
      */
